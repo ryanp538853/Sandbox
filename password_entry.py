2ba3378661a175c341407411f2fe2562a1441440
ryanp538853 <ryan.pauku@my.jcu.edu.au>
2019-08-22 12:22:00 +1000

    add password check

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,8 @@
 """Ryan"""
+MINIMUM_LENGTH = 6
+
+password = input("Please enter your password that has at least {} character: ".format(MINIMUM_LENGTH))
+while len(password) < MINIMUM_LENGTH:
+    input("Invalid password!\nPlease enter password that contains at least {} characters: ".format(MINIMUM_LENGTH))
+
+print("*" * len(password))
